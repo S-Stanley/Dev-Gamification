012e0e0708c49263921505785d7507c54ae2484e
Stanley Serbin <stanleyserbin@gmail.com>
2021-11-01 00:14:59 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, redirect, request, jsonify
+from utils import gitlab
+from flask_pymongo import PyMongo
+import os, datetime
+
+app = Flask(__name__)
+
+app.config['MONGO_URI'] = 'mongodb+srv://admin:{}@cluster0.tey73.mongodb.net/dev?retryWrites=true&w=majority'.format(os.environ['db_password'])
+mongo = PyMongo(app)
+
+def add_stats(username: str, count: int):
+	mongo.db.data.insert_one({
+		'username': username,
+		'total': count,
+	})
+
+def find_merge(merge_id: str):
+	print(merge_id)
+	to_find = mongo.db.merges.find_one({
+		'id': merge_id,
+	})
+	print(to_find)
+	return to_find
+
+def find_project(project_id: str):
+	to_find = mongo.db.projects.find_one({
+		'id': project_id,
+	})
+	return to_find
+
+def find_user_by_email(email: str):
+	to_find = mongo.db.users.find_one({
+		'email': email
+	})
+	return to_find
+
+def create_user(usr, auth):
+	if find_user_by_email(usr['email']): return
+	mongo.db.users.insert_one({
+		'name': usr['name'],
+		'username': usr['username'],
+		'avatar_url': usr['avatar_url'],
+		'web_url': usr['web_url'],
+		'email': usr['email'],
+		'refresh_token': auth['refresh_token'],
+		'access_token': auth['access_token'],
+		'created_at': str(datetime.datetime.now())
+	})
+
+def create_merge(merge):
+	if find_merge(merge['id']): return
+	mongo.db.merges.insert_one(merge)
+
+def create_project(project):
+	if find_project(project['id']): return
+	mongo.db.projects.insert_one(project)
+
+
+@app.route('/')
+def hello_word():
+	code = request.args.get('code')
+	info_token = gitlab.get_token(code)
+	'''
+	To store in database:
+		- info_token.access_token
+		- info_token.refresh_token
+	'''
+	all_projects = gitlab.get_all_project_by_user(info_token['access_token'])
+	for project in all_projects:
+		create_project(project)
+	all_merges = gitlab.get_all_merge_request_by_project_id(info_token['access_token'])
+	for merge in all_merges:
+		create_merge(merge)
+	ladder = gitlab.count_merges()
+	for player in ladder:
+		add_stats(player['username'], player['merges'])
+	create_user(gitlab.get_user_info(info_token['access_token']), info_token)
+	return jsonify(ladder)
+
+@app.route('/link')
+def get_auth_link():
+	return redirect(gitlab.get_auth_uri())
---
+++ utils/gitlab.py
@@ -0,0 +1,87 @@
+from dotenv import load_dotenv
+import os, requests, json
+from tqdm import tqdm
+
+load_dotenv()
+
+def find_author_or_create(arr, item):
+	for tmp in arr:
+		if tmp['username'] == item['author']['username']:
+			tmp['merges'].append(item)
+			return
+	arr.append({
+		'username': item['author']['username'],
+		'merges': [item]
+	})
+
+def sort_by_merges_number(e):
+	return len(e['merges'])
+
+def count_merges():
+	with open('merges.json', 'r') as f:
+		data = json.loads(f.read())
+	output = []
+	for merge in tqdm(data):
+		if 'state' in merge and merge['state'] == 'merged':
+			find_author_or_create(output, merge)
+	output.sort(reverse=True, key=sort_by_merges_number)
+	to_return = []
+	for i in output:
+		to_return.append({
+			'username': i['username'],
+			'merges': len(i['merges'])
+		})
+	return to_return
+
+def get_all_merge_request_by_project_id(access_token):
+	with open('projects.json', 'r') as f:
+		projects = json.loads(f.read())
+	output = []
+	for project in projects:
+		items_get = 100
+		page = 0
+		while items_get == 100:
+			page += 1
+			url = 'https://gitlab.com/api/v4/projects/{}/merge_requests?access_token={}&per_page=100&page={}'.format(project['id'], access_token, page)
+			req = requests.get(url)
+			items_get = len(req.json())
+			for i in req.json():
+				output.append(dict(i))
+	with open('merges.json', 'w') as f:
+		f.write(json.dumps(output))
+	return output
+
+
+def get_user_info(access_token):
+	url = 'https://gitlab.com/api/v4/user?access_token={}'.format(access_token)
+	req = requests.get(url)
+	return (req.json())
+
+def get_all_project_by_user(access_token):
+	url = 'https://gitlab.com/api/v4/projects?access_token={}&owned=true'.format(access_token)
+	req = requests.get(url)
+	with open('projects.json', 'w') as f:
+		f.write(json.dumps(req.json()))
+	return req.json()
+
+def get_auth_uri():
+	url = 'https://gitlab.com/oauth/authorize?client_id={}&redirect_uri={}&response_type=code&state={}&scope={}'.format(
+		os.environ['client_id'],
+		os.environ['redirect_uri'],
+		os.environ['state'],
+		os.environ['scope'],
+	)
+	return url
+
+def get_token(code):
+	data = {
+		'client_id': os.environ['client_id'],
+		'code' : code,
+		'grant_type': 'authorization_code',
+		'redirect_uri': os.environ['redirect_uri'],
+		'client_secret': os.environ['client_secret'],
+	}
+	url = f'https://gitlab.com/oauth/token'
+	req = requests.post(url, data)
+	print(req.status_code, req.text)
+	return req.json()
