2905bf362c1807cb58dc04ea210a1261a61ccc61
Stanley Serbin <stanleyserbin@gmail.com>
2021-11-03 23:45:24 +0100

    PUSH made by pusher https://github.com/S-Stanley/pusher.git

--- utils/gitlab.py
+++ utils/gitlab.py
@@ -21,7 +21,7 @@ def get_grad(nb_merge):
 	with open('./grad.json', 'r') as f:
 		all_grades = json.loads(f.read())
 	for grade in all_grades:
-		if grade['number'] < nb_merge:
+		if int(grade['number']) < nb_merge:
 			return grade['name']
 
 def count_merges(data):
