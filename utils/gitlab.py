62a06faa60dc21ae11f74a45273e2ec3a7176044
Stanley Serbin <stanleyserbin@gmail.com>
2021-11-03 22:38:21 +0100

    PUSH made by pusher https://github.com/S-Stanley/pusher.git

--- utils/gitlab.py
+++ utils/gitlab.py
@@ -42,6 +42,7 @@ def get_all_merge_request_by_project_id(access_token):
 		page = 0
 		while items_get == 100:
 			page += 1
+			print(project['id'], access_token, page)
 			url = 'https://gitlab.com/api/v4/projects/{}/merge_requests?access_token={}&per_page=100&page={}'.format(project['id'], access_token, page)
 			req = requests.get(url)
 			items_get = len(req.json())
